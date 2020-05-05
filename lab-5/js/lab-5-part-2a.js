74bd886621535008a9cc53873d97fdbe7c992b64
Charles Advincula <chadvincula7636428@gmail.com>
2020-05-04 17:19:18 -0700

    Finished part 2a
    - Each particle grows and shrinks in size
    - Each particle also constantly changes hue
    - Each particle also uses a circle sprite/texture

--- lab-5/js/lab-5-part-2a.js
+++ lab-5/js/lab-5-part-2a.js
@@ -6,8 +6,83 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 camera.position.z = 60;
 
+// Creating a Billboard for particles
+var vertices = [];
+
+for ( var i = 0; i < 1000; i ++ ) {
+    var x = THREE.MathUtils.randFloatSpread( 500 );
+    var y = THREE.MathUtils.randFloatSpread( 500 );
+    var z = THREE.MathUtils.randFloatSpread( 500 );
+    vertices.push( x, y, z );
+}
+
+var geometry = new THREE.BufferGeometry();
+geometry.setAttribute("position", new THREE.Float32BufferAttribute(vertices, 3));
+var texture = new THREE.TextureLoader().load("textures/disc.png.");
+var material = new THREE.PointsMaterial( { map: texture,
+                                           size: 40,
+                                           alphaTest: 0.5,
+                                           transparent: true } );
+
+var points = new THREE.Points(geometry, material);
+scene.add(points);
+
+// Function taken from three.js_shadowmap_pointlight example
+// https://github.com/mrdoob/three.js/blob/master/examples/webgl_shadowmap_pointlight.html
+function onWindowResize()
+{
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+window.addEventListener( 'resize', onWindowResize, false );
+
+// Adding interactivity
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+var mouseX = 0;
+var mouseY = 0;
+
+function onDocumentMouseMove(event)
+{
+    mouseX = event.clientX - windowHalfX;
+    mouseY = event.clientY - windowHalfY;
+}
+
+function onDocumentTouchStart(event)
+{
+    if(event.touches.length == 1)
+    {
+        event.preventDefault();
+        mouseX = event.touches[0].pageX - windowHalfX;
+        mouseY = event.touches[0].pageY - windowHalfY;
+    }
+}
+
+function onDocumentTouchMove(event)
+{
+    if (event.touches.length == 1)
+    {
+        event.preventDefault();
+        mouseX = event.touches[0].pageX - windowHalfX;
+        mouseY = event.touches[0].pageY - windowHalfY;
+    }
+}
+
+document.addEventListener('mousemove', onDocumentMouseMove, false);
+document.addEventListener('touchstart', onDocumentTouchStart, false);
+document.addEventListener('touchmove', onDocumentTouchMove, false);
+
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+
+    // Animate particle texture
+    material.color.setHSL(performance.now() / 1500, 0.5, 0.5);
+    material.size = (Math.cos(performance.now() / 1500) + 1.1) * 40;
+
+    // Mouse-camera functionality
+    camera.position.x += (mouseX - camera.position.x) * 0.005;
+    camera.position.y += (-mouseY - camera.position.y) * 0.005;
 }
 animate();
