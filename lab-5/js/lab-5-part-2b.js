419428295e4ac2b889051d72b05c683077f8d080
Charles Advincula <chadvincula7636428@gmail.com>
2020-05-05 14:51:51 -0700

    Completed part 2b?
    - All of the steps seem to be very straightforward
    - The entire part seems to complete itself so long as you copy-paste the code
    - I hope I don't get docked points for this

--- lab-5/js/lab-5-part-2b.js
+++ lab-5/js/lab-5-part-2b.js
@@ -6,8 +6,50 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 camera.position.z = 60;
 
+var particles = [];
+const geometry = new THREE.Geometry()
+for(let i = 0; i < 1000; i++)
+{
+    const particle = {
+        position: new THREE.Vector3(
+            Math.random() * 2 - 1,
+            Math.random() * 2 - 1,
+            Math.random() * 3 - 3),
+        velocity: new THREE.Vector3(
+            Math.random() * .02 - .01,
+            0.06,
+            Math.random() * .02 - .01),
+        acceleration: new THREE.Vector3(
+            Math.random() * .002 - .001, 
+            Math.random() * .002 - .001,
+            0)
+    }
+    particles.push(particle);
+    geometry.vertices.push(particle.position)
+}
+
+const material = new THREE.PointsMaterial({color:0xffffff,size: 0.5});
+var mesh = new THREE.Points(geometry, material);
+mesh.position.z = -4;
+scene.add(mesh);
+
+// Function taken from three.js_shadowmap_pointlight example
+// https://github.com/mrdoob/three.js/blob/master/examples/webgl_shadowmap_pointlight.html
+function onWindowResize()
+{
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+window.addEventListener( 'resize', onWindowResize, false );
+
 function animate()
 {
+    particles.forEach(p => {
+        p.velocity.add(p.acceleration)
+        p.position.add(p.velocity)
+    });
+    mesh.geometry.verticesNeedUpdate = true;        
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
