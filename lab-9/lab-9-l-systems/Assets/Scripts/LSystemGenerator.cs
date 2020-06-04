b340b49c457661478a3f83fa08d0e616dbda3648
Charles Advincula <chadvincula7636428@gmail.com>
2020-06-03 22:12:42 -0700

    Finished tutorial @ 10min mark

---
+++ lab-9/lab-9-l-systems/Assets/Scripts/LSystemGenerator.cs
@@ -0,0 +1,44 @@
+﻿// Code taken from L-Systems Tutorial by Richard Hawkes:
+// https://youtu.be/uBEA6VSUybk
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LSystemGenerator : MonoBehaviour
+{
+    private string currentString;
+    private string axiom = "A";
+    private Dictionary<char, string> rules = new Dictionary<char, string>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentString = axiom;
+        rules.Add('A', "AB");
+        rules.Add('B', "A");
+
+        Generate();
+        Generate();
+        Generate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Generate()
+    {
+        string modifiedString = "";
+
+        char[] currentStringAsChars = currentString.ToCharArray();
+        for(int i = 0; i < currentStringAsChars.Length; i++)
+        {
+            modifiedString += rules[currentStringAsChars[i]];
+        }
+
+        currentString = modifiedString;
+        Debug.Log(currentString);
+    }
+}
